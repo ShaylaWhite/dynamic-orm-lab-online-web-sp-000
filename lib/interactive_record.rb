e64547c24070c0054543b15e926257e959535b8e
Shayla White <shayladwhite1@gmail.com>
2020-01-26 05:31:36 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,7 +7,16 @@ class InteractiveRecord
     "#{self.to_s.downcase}s"
    end
    
-   def self.column_names
-   end
+  def self.column_names
 
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+  
 end
